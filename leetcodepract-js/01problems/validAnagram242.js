516d5e22f42bc7a9a90d83a187f86f45fa4e1e04
Thiem Huynh <thiem.huynh0@gmail.com>
2024-02-08 13:31:55 -0500

    valid anagram writing

--- leetcodepract-js/01problems/validAnagram242.js
+++ leetcodepract-js/01problems/validAnagram242.js
@@ -4,13 +4,22 @@
  * @return {boolean}
  */
 // source: https://leetcode.com/problems/valid-anagram/
+// O(n^2) operation
 
 var isAnagram = function (s, t) {
-  let map1 = new Map();
-  let map2 = new Map();
+  if (s.length !== t.length) {
+    return false; //can't be anagram is not same length
+  }
 
-  console.log(map1);
-  console.log(map2);
+  const letterMap = new Map();
+
+  // count letters of s into a map
+  for (let char of s) {
+    let count = letterMap.get(char) || 0; //if found 1, else 0
+    letterMap.set(char, count + 1); //increment leter if found
+  }
+
+  // subtract items of map s from chars of t
 };
 
 module.exports = isAnagram;
