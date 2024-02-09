d5927073055b1371ff15727ad5b96fb690f42a64
Thiem <thiem.huynh0@gmail.com>
2024-02-09 14:10:25 -0500

    twosum complete

--- leetcodepract-js/01problems/twosum1.js
+++ leetcodepract-js/01problems/twosum1.js
@@ -4,9 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  for (num in nums) {
-    console.log(num);
+  const numIndex = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = Math.abs(nums[i] - target);
+    if (numIndex.hasOwnProperty(complement)) {
+      return [numIndex[complement], i]; //two sums found, return indexes
+    }
+    numIndex[nums[i]] = i; //add to index
   }
+  return false; //no twosums found
 };
 
 module.exports = twoSum;
--- leetcodepract-js/tests/twoSum.test.js
+++ leetcodepract-js/tests/twoSum.test.js
@@ -3,14 +3,14 @@ const twoSum = require("../01problems/twosum1");
 describe("twoSum", () => {
   let nums = null;
   let target = null;
-  it("return true if sums exist", () => {
+  it("return [2,7] if sums exist", () => {
     nums = [2, 7, 11, 15];
     target = 9;
-    expect(twoSum(nums, target)).toBe(false);
+    expect(twoSum(nums, target)).toEqual([0, 1]);
   });
   it("return false if no sum exists", () => {
     nums = [2, 7, 11, 15];
     target = 3;
-    expect(twoSum(nums, target)).toBe(true);
+    expect(twoSum(nums, target)).toBe(false);
   });
 });
