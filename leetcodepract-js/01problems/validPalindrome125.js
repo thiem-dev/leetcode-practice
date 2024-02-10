54cfb54c6d3421c45028e1915d4d9abe53e4bb37
Thiem <thiem.huynh0@gmail.com>
2024-02-10 18:09:56 -0500

    setup for validpalidrome

---
+++ leetcodepract-js/01problems/validPalindrome125.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    const cleanedString = s.replace(/\s+/g, '').toLowerCase();
+    const leftpt, rightpt;
+
+};
+
+
+module.exports= isPalindrome;
---
+++ leetcodepract-js/tests/validpalindrome.test.js
@@ -0,0 +1,1 @@
+// const fx = require();
