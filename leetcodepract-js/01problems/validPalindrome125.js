a2569b91c73ae0faa30f66f41e6104d0b7782ce9
Thiem <thiem.huynh0@gmail.com>
2024-02-10 20:53:40 -0500

    shifted the comments

--- leetcodepract-js/01problems/validPalindrome125.js
+++ leetcodepract-js/01problems/validPalindrome125.js
@@ -3,15 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  // remove non-alphanumeric characters
   if (s === " ") {
     return true;
   }
+
+  // remove non-alphanumeric characters and spaces
   const newStr = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
   let left = 0;
   let right = newStr.length - 1;
 
+  // left pointer moves up as right pointer moves backwards, compare till complete
   while (left < right) {
     if (newStr[left] !== newStr[right]) {
       return false;
