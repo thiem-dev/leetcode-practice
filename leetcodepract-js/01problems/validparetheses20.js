c60efbcac622803a3cc92ec2399dfa0c88ef1873
Thiem Huynh <thiem.huynh0@gmail.com>
2024-03-01 17:23:57 -0500

    validparentheses progress

--- leetcodepract-js/01problems/validparetheses20.js
+++ leetcodepract-js/01problems/validparetheses20.js
@@ -2,6 +2,28 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {};
+var isValid = function (s) {
+  if (s === "" || s === " " || !s) {
+    return false; //empty strings
+  }
+
+  const stack = [];
+  const bracketPairs = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    char = s[i];
+    if (bracketPairs[char]) {
+      stack.push(char); //if opening brack, push char to stack
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+    }
+  }
+};
 
 module.exports = isValid;
--- leetcodepract-js/tests/validParentheses.test.js
+++ leetcodepract-js/tests/validParentheses.test.js
@@ -1,9 +1,17 @@
-const isValid = require("../01problems/validparetheses20.js");
+const isValid = require("../01problems/validparetheses20");
 
 describe("isValidParentheses", () => {
   let s = null;
   it("return true if () is valid parentheses pair", () => {
     s = "()";
     expect(isValid(s)).toBe(true);
+  });
+  it("return true if ()[]{} is valid parentheses pairs", () => {
+    s = "()[]{}";
+    expect(isValid(s)).toBe(true);
+  });
+  it("return false if (] invalid parentheses pairs", () => {
+    s = "(]";
+    expect(isValid(s)).toBe(false);
   });
 });
