aba004845ea1f7b0e91edaa2559de62126b1fde7
Thiem Huynh <thiem.huynh0@gmail.com>
2024-03-02 09:07:42 -0500

    binary search complete, need to write the tests

--- leetcodepract-js/01problems/binarySearch704.js
+++ leetcodepract-js/01problems/binarySearch704.js
@@ -5,21 +5,26 @@
  */
 var search = function (nums, target) {
   // divide the array in half (rounded down) and test index if greater or less than. loop through left array or right array;
-  const foundIndex = searchHelper(nums, target);
+  const foundIndex = searchHelper(nums, target, 0, nums.length - 1);
   return foundIndex;
 
-  function searchHelper(halfArr, target) {
-    let middleIndex = Math.floor(nums.length / 2);
-    if (nums[middleIndex] === target) {
-      return nums[middleIndex]; //foundIndex
+  function searchHelper(nums, target, left, right) {
+    if (left > right) {
+      return -1; //not found
     }
-    //less than
-    if (nums[middleIndex] < target) halfArr = nums.slice(0, middleIndex);
-    searchHelper(halfArr, target);
-    //greater than
+
+    let middleIndex = Math.floor((left + right) / 2);
+
+    if (nums[middleIndex] === target) {
+      return middleIndex; //foundIndex
+    }
+
     if (nums[middleIndex] > target) {
-      halfArr = nums.slice(middleIndex, nums.length - 1);
-      searchHelper(halfArr, target);
+      return searchHelper(nums, target, left, middleIndex - 1); //search left
+    } else {
+      return searchHelper(nums, target, middleIndex + 1, right); //search right
     }
   }
 };
+
+module.exports = search;
