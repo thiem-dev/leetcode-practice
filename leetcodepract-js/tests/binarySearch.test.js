1f2e9cc1812a094f0279323b6c43de1887fc6ba9
Thiem Huynh <thiem.huynh0@gmail.com>
2024-03-06 16:08:20 -0500

    test update

--- leetcodepract-js/tests/binarySearch.test.js
+++ leetcodepract-js/tests/binarySearch.test.js
@@ -2,9 +2,14 @@ const search = require("../01problems/binarySearch704");
 
 describe("binarySearch", () => {
   let nums, target;
-  it("return true if target exists", () => {
+  it("return index of num if target exists", () => {
     nums = [-1, 0, 3, 5, 9, 12];
     target = 9;
     expect(search(nums, target)).toEqual(4);
+  });
+  it("return -1 if target does not exist", () => {
+    nums = [-1, 0, 3, 5, 9, 12];
+    target = 2;
+    expect(search(nums, target)).toEqual(-1);
   });
 });
